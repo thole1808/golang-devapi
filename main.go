b1fb4e28002920c648a96e441a8c7b1fa861ab42
achmadagus <achmadagushadiyanto18@gmail.com>
2023-01-02 11:44:45 +0700

    update getId

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 
 	// buatlah route anda masing - masing sesuai kebutuhan
 	router.GET("/", rootHandler)
+	router.GET("/getid/:id", getIdHandler)
 
 	router.Run(":8181")
 }
@@ -19,4 +20,9 @@ func rootHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"data": "null",
 	})
+}
+
+func getIdHandler(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"id": id})
 }
