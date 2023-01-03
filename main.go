861964a796284892319aeac6ef7b46eacfe99a31
achmadagus <achmadagushadiyanto18@gmail.com>
2023-01-03 10:03:17 +0700

    update

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"golang-devapi/handler"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
+
+var Database *gorm.DB
 
 func main() {
 	router := gin.Default()
@@ -23,6 +26,24 @@ func main() {
 
 	// Post Data
 	router.POST("/post", handler.PostHandler)
+
+	// Konfigurasi Database
+	// var err error
+	// host := os.Getenv("DB_HOST")
+	// username := os.Getenv("DB_USER")
+	// password := os.Getenv("DB_PASSWORD")
+	// databaseName := os.Getenv("DB_NAME")
+	// port := os.Getenv("DB_PORT")
+
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos", host, username, password, databaseName, port)
+	// Database, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	panic(err)
+	// } else {
+	// 	fmt.Println("Successfully connected to the database")
+	// }
+	// Konfigurasi Database
 
 	// run port
 	router.Run(":8282")
